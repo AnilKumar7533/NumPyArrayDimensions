9be89c41b048bf82d37e5d17747d72e8a3eebe5b
Anilkumar123-github <beeslittle08@gmail.com>
2026-01-09 23:35:53 +0530

    First commit

---
+++ onedimension.py
@@ -0,0 +1,5 @@
+import numpy as np;
+
+n = np.array([1,4,5]);
+print(n)
+print("Dimensions =",n.ndim)
---
+++ twodimension.py
@@ -0,0 +1,7 @@
+import numpy as np;
+
+n = np.array([[1,2,3],[4,5,6]])
+#array with two rows and three columns
+
+print(n)
+print("Dimensions =",n.ndim);
